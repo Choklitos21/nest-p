6b6da36d384de97c2ce2c46375f1e26706477d17
Diego Morales <diego.mm09@hotmail.com>
2022-07-22 18:36:47 -0500

    0.0001

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const PORT = parseInt(process.env.PORT, 10) || 3000; await app.listen(PORT);
+  const PORT = parseInt(process.env.PORT, 10) || 3000;
 
-  
+
   await app.listen(PORT);
 }
 bootstrap();
