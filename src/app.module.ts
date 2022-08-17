76ec4e87c03801b51061a619b35910679e64a8f8
DiegoMorales <diego.mm09@hotmail.com>
2022-08-16 20:58:01 -0500

    Something

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
 import { PetController } from './pet/pet.controller';
 import { HttpModule } from "@nestjs/axios";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from 'path';
+
 
 @Module({
   imports: [HttpModule.registerAsync({
@@ -11,7 +14,10 @@ import { HttpModule } from "@nestjs/axios";
       timeout: 5000,
       maxRedirects: 5,
     }),
-  })],
+  }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),],
   controllers: [AppController, HealthController, PetController],
   providers: [AppService],
 })
